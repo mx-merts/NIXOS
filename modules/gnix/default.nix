0012119d01e92124d2c84e2c6e4c55b2f90962ab
m <m@nixos>
2026-05-25 12:59:39 +0300

    gnix: wine staging, dxvk, vkd3d, unity, java

--- modules/gnix/default.nix
+++ modules/gnix/default.nix
@@ -11,13 +11,17 @@
     nvidiaSettings = true;
     powerManagement.enable = true;
     powerManagement.finegrained = true;
-
     prime = {
       offload.enable = true;
       offload.enableOffloadCmd = true;
       intelBusId = "PCI:0:2:0";
       nvidiaBusId = "PCI:1:0:0";
     };
+  };
+
+  xdg.portal = {
+    enable = true;
+    xdgOpenUsePortal = true;
   };
 
   nixpkgs.config.packageOverrides = super: {
@@ -35,7 +39,22 @@
   programs.gamemode.enable = true;
 
   environment.systemPackages = with pkgs; [
-    lutris heroic mangohud protonup-qt
+    lutris
+    heroic
+    bottles
+    wineWowPackages.stagingFull
+    winetricks
+    cabextract
+    dxvk
+    vkd3d-proton
+    vulkan-tools
+    vulkan-validation-layers
+    vulkan-extension-layer
+    mangohud
+    gamescope
+    protonup-qt
     nvtopPackages.full
+    unityhub
+    jdk21
   ];
 }
