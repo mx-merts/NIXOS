1f9c0871b19b822fab993883e5ddbd5aa9473f41
mx-merts <mxmerts33@gmail.com>
2026-05-29 23:55:08 +0300

    webdev: syntax duzeltildi

--- modules/webdev/default.nix
+++ modules/webdev/default.nix
@@ -14,46 +14,44 @@
   };
 
   # ---------------------------------------------------------------- #
-  # 2. APACHE + PHP                                                   #
+  # 2. APACHE + PHP + PHPMYADMİN                                     #
   # ---------------------------------------------------------------- #
- services.httpd = {
-      enable     = true;
-      enablePHP  = true;
-      phpPackage = pkgs.php83;
-      adminAddr  = "admin@localhost";
-  
-      virtualHosts."localhost" = {
-        documentRoot = "/srv/http";
-        extraConfig  = ''
-          <Directory "/srv/http">
-            AllowOverride All
-            Require all granted
-          </Directory>
-  
-          Alias /phpmyadmin ${pkgs.phpmyadmin}/share/phpmyadmin
-          <Directory "${pkgs.phpmyadmin}/share/phpmyadmin">
-            AllowOverride All
-            Require all granted
-          </Directory>
-        '';
-      };
-      };
+  services.httpd = {
+    enable     = true;
+    enablePHP  = true;
+    phpPackage = pkgs.php83;
+    adminAddr  = "admin@localhost";
+
+    virtualHosts."localhost" = {
+      documentRoot = "/srv/http";
+      extraConfig  = ''
+        <Directory "/srv/http">
+          AllowOverride All
+          Require all granted
+        </Directory>
+
+        Alias /phpmyadmin ${pkgs.phpmyadmin}/share/phpmyadmin
+        <Directory "${pkgs.phpmyadmin}/share/phpmyadmin">
+          AllowOverride All
+          Require all granted
+        </Directory>
+      '';
     };
-
-
+  };
 
   # ---------------------------------------------------------------- #
-  # 4. /srv/http klasörü — m_merts yazsın diye                       #
+  # 3. /srv/http klasörü — m_merts yazsın diye                       #
   # ---------------------------------------------------------------- #
   systemd.tmpfiles.rules = [
     "d /srv/http 0755 m_merts users -"
   ];
 
   # ---------------------------------------------------------------- #
-  # 5. .NET SDK                                                       #
+  # 4. .NET SDK                                                       #
   # ---------------------------------------------------------------- #
- environment.systemPackages = with pkgs; [
+  environment.systemPackages = with pkgs; [
     dotnet-sdk_8
     mariadb
     phpmyadmin
   ];
+}
